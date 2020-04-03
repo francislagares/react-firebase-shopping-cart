8ed96346e3768e9b274a54545c84f5e5e508e3b7
Francis Lagares <francis.lagares@gmail.com>
2020-04-03 19:22:08 +0200

    Created shop page & collection preview components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MenuItem from '../menu-items/menu-item.component';
 import './directory.styles.scss';
+import SECTIONS_DATA from './sections.data';
 
 class Directory extends Component {
 
@@ -8,43 +9,7 @@ class Directory extends Component {
     super(props);
 
     this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          size: 'large',
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          size: 'large',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          size: 'large',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
+      sections: SECTIONS_DATA
     }
   }
 
