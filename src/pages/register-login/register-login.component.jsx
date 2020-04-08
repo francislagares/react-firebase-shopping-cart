18c6f565006160795944dcae75a3a059497914da
Francis Lagares <francis.lagares@gmail.com>
2020-04-08 14:30:02 +0200

    Created sign-up & sign-in page

---
+++ src/pages/register-login/register-login.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './register-login.styles.scss';
+
+const RegisterLogin = () => (
+  <div className='register-login'>
+    <SignIn />
+  </div>
+);
+
+export default RegisterLogin;
