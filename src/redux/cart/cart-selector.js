497f8b68365bbb531e8e67f08b2f7b7d426905cf
Francis Lagares <francis.lagares@gmail.com>
2020-04-14 19:25:39 +0200

    update: cart to use selectors from reselect npm

--- src/redux/cart/cart-selector.js
+++ src/redux/cart/cart-selector.js
@@ -2,6 +2,11 @@ import { createSelector } from 'reselect';
 
 const selectCart = (state) => state.cart;
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden
+);
+
 export const selectCartItems = createSelector(
   [selectCart],
   (cart) => cart.cartItems
@@ -14,4 +19,12 @@ export const selectCartItemsCount = createSelector(
       (accumlatedQuantity, cartItem) => accumlatedQuantity + cartItem.quantity,
       0
     )
+);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumlatedQuantity, cartItem) =>
+      accumlatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
 );
