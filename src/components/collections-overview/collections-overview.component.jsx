0811c711af04de8201ef753573b60071c531d6c2
Francis Lagares <francis.lagares@gmail.com>
2020-06-04 17:24:24 +0200

    refactor: data normalization for collection page
    Moved directory and shop data into its own reducers.
    Create respective selectors.
    Update corresponding components with new redux flow for directory/shop.

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
+  <div className="collections-overview">
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
@@ -16,7 +16,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,9 +7,12 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = (collectionUrlParams) =>
   createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParams]
-    )
+    collections[collectionUrlParams]
   );
